0edbb4acb6606c2e65fa190d9626cdcdc21f6cd6
Unknown <chouania.mehdi@hotmail.fr>
2017-05-22 19:55:40 +0200

    Page de contact 4.0

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('public/contact.html.twig');
+    }
+}
